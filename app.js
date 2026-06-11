d54536d5e6aa9375bce9089d33b29cce3bc962e7
yogesh1006 <yogeshkasliwal707@gmail.com>
2026-06-11 21:08:00 +0530

    task apis

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use("/", require("./routes/taskRoutes"));
 app.listen(port, (err) => {
-    if (err) {
-        return console.log('Something bad happened', err);
-    }
-    console.log(`Server is listening on ${port}`);
+	if (err) {
+		return console.log("Something bad happened", err);
+	}
+	console.log(`Server is listening on ${port}`);
 });
-
-
 
 module.exports = app;
